13ba6aa459a20b389dcfdca8a85ed6259ac13134
jssomerville <jssomerville2@gmail.com>
2016-12-06 05:03:48 -0500

    added button

--- closedElections.php
+++ closedElections.php
@@ -103,9 +103,11 @@
 									echo "<button type='submit' class='btn btn-primary btn-lg' name='selectedElection' value='{$electionBallot["title"]}'>Go to {$electionBallot["title"]} results page</button>";
 									echo "</form>";
 									echo "<form action='#' method='post'>";
+                                    echo "<div class='row'>"
 									echo "<div class='text-right'>";
 									echo "<button type='submit' class='btn btn-danger btn-md' id='closeBtn{$counter}' name='selectedElection' value='{$electionBallot["title"]}'>Certify {$electionBallot["title"]}</button>";
-									echo "</div></form></li>";
+                                    echo "<button type='submit' class='btn btn-danger btn-md' id='' name'' value=''>Results</button>";
+									echo "</div></div></form></li>";
 								}
 							}
 							$counter++;
